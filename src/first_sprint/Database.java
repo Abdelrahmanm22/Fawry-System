25606fa3a5152df700088947289fb3225773a8f1
Abdelrahmanm22 <abdra1396@gmail.com>
2022-11-27 23:31:19 +0200

    F

--- src/first_sprint/Database.java
+++ src/first_sprint/Database.java
@@ -9,7 +9,7 @@ public class Database {
 
 	private static Database instance = new Database();
 	private Scanner scanner;
-	private static File file = new File("D:\\F  C  I\\Fawry_System\\Users.txt");
+	private static File file = new File("D:\\University\\Advanced SW\\Project\\Phase1\\FawrySystem\\Users.txt");
 	
 	private Database() {
 	}
